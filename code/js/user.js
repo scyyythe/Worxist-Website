904f6c253651abebdbb405fa8fd773cfdb677616
scyyythe <caneteangel187@gmail.com>
2024-09-16 13:03:43 +0800

    Artwork Dashboard

---
+++ code/js/user.js
@@ -0,0 +1,10 @@
+
+// dropdown filter
+function toggleDropdown() {
+    var dropdown = document.getElementById("dropdown");
+    if (dropdown.style.display === "block") {
+        dropdown.style.display = "none";
+    } else {
+        dropdown.style.display = "block";
+    }
+}
