6a8365a4d61c0e4a6ce663ad76f7895e5e8e148f
scyyythe <caneteangel187@gmail.com>
2024-09-20 00:37:52 +0800

    dasboard

--- code/js/user.js
+++ code/js/user.js
@@ -1,26 +1,131 @@
 
+document.addEventListener('DOMContentLoaded', function() {
+
+  // sidebar links
+  const dashboardLink = document.querySelector('.dashboard');
+  const artworkLink = document.querySelector('.my-artworks');
+  const messageLink=document.querySelector('.messages');
+  const notificationLink=document.querySelector('.notification-container');
+  const exhibitLink=document.querySelector('.exhibit');
+  const settingLink=document.querySelector('.settings');
+
+  
+  // containers
+  const dashboardContainer = document.getElementById('dashboardContainer');
+  const artworkContainer = document.getElementById('artworkContainer');
+  const messageContainer = document.getElementById('messageContainer');
+  const notificationContainer = document.getElementById('notificationContainer');
+  const exhibitContainer = document.getElementById('exhibitContainer');
+  const settingContainer = document.getElementById('settingsContainer');
+
+  //  show only the dashboard and hide others
+  artworkContainer.style.display = 'none';
+  messageContainer.style.display = 'none';
+  notificationContainer.style.display = 'none';
+  exhibitContainer.style.display = 'none';
+  settingContainer.style.display = 'none';
+
+
+  // Dashboard
+  dashboardLink.addEventListener('click', function(e) {
+      e.preventDefault(); 
+      dashboardContainer.style.display = 'block';
+      artworkContainer.style.display = 'none';
+      messageContainer.style.display = 'none';
+      notificationContainer.style.display = 'none';
+      exhibitContainer.style.display = 'none';
+      settingContainer.style.display = 'none';
+
+  });
+
+  //my-artworks Management
+  artworkLink.addEventListener('click', function(e) {
+      e.preventDefault();
+      dashboardContainer.style.display = 'none';
+      artworkContainer.style.display = 'block';
+      messageContainer.style.display = 'none';
+      notificationContainer.style.display = 'none';
+      exhibitContainer.style.display = 'none';
+      settingContainer.style.display = 'none';
+
+  });
+
+  // messages section
+messageLink.addEventListener('click', function(e) {
+    e.preventDefault();
+    dashboardContainer.style.display = 'none';
+    artworkContainer.style.display = 'none';
+    messageContainer.style.display = 'block';
+    notificationContainer.style.display = 'none';
+    exhibitContainer.style.display = 'none';
+    settingContainer.style.display = 'none';
+
+});
+
+// notification section
+notificationLink.addEventListener('click', function(e) {
+  e.preventDefault();
+     dashboardContainer.style.display = 'none';
+      artworkContainer.style.display = 'none';
+      messageContainer.style.display = 'none';
+      notificationContainer.style.display ='block';
+      exhibitContainer.style.display = 'none';
+      settingContainer.style.display = 'none';
+
+});
+
+
+// exhibit container
+exhibitLink.addEventListener('click', function(e) {
+  e.preventDefault();
+  dashboardContainer.style.display = 'none';
+      artworkContainer.style.display = 'none';
+      messageContainer.style.display = 'none';
+      notificationContainer.style.display = 'none';
+      exhibitContainer.style.display = 'block';
+      settingContainer.style.display = 'none';
+
+});
+
+
+// settings
+settingLink.addEventListener('click', function(e) {
+  e.preventDefault();
+  dashboardContainer.style.display = 'none';
+      artworkContainer.style.display = 'none';
+      messageContainer.style.display = 'none';
+      notificationContainer.style.display = 'none';
+      exhibitContainer.style.display = 'none';
+      settingContainer.style.display = 'block';
+
+});
+
+
+   
+});
 
 // tabpane in Artwork Dashboard
 function openCity(evt, cityName) {
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    document.getElementById(cityName).style.display = "block";
-    evt.currentTarget.className += " active";
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
   }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
 
 // dropdown filter
 function toggleDropdown() {
-    var dropdown = document.getElementById("dropdown");
-    if (dropdown.style.display === "block") {
-        dropdown.style.display = "none";
-    } else {
-        dropdown.style.display = "block";
-    }
+  var dropdown = document.getElementById("dropdown");
+  if (dropdown.style.display === "block") {
+      dropdown.style.display = "none";
+  } else {
+      dropdown.style.display = "block";
+  }
 }
+
